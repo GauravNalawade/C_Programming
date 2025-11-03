d967fb964f86cc8eedce0f51a7f84a35bbed1638
GauravNalawade <gauravnakawade11@gmail.com>
2025-11-03 22:08:39 +0530

    Add Assignment_2 solutions (Q1-

---
+++ Assignments/Assignment_2/program2_4.c
@@ -0,0 +1,28 @@
+// Acccept two numbers from user and display first number in second number of times.
+#include <stdio.h>
+void Display(int iNo, int iFrequency)
+{
+
+    if (iFrequency < 0)
+    {
+        iFrequency = -iFrequency;
+    }
+    int i = 0;
+    for (i = 0; i < iFrequency; i++)
+    {
+        printf("%d\t", iNo);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+    printf("Enter number");
+    scanf("%d", &iValue);
+    printf("Enter Frequency");
+    scanf("%d", &iCount);
+
+    Display(iValue, iCount);
+
+    return 0;
+}
---
+++ Assignments/Assignment_2/program2_5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool ChkEven(int iNo)
+{
+    if (iNo % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+    printf("Enter Number");
+    scanf("%d", &iValue);
+    bRet = ChkEven(iValue);
+
+    if (bRet == true)
+    {
+        printf("%d is  is Even number", iValue);
+    }
+    else
+    {
+        printf("%d is  Odd number", iValue);
+    }
+
+    return 0;
+}
